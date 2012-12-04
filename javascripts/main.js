3d461f662991ba0f0c448ed9b0f5e30914c2bf4f
Christian Hofstaedtler <christian@hofstaedtler.name>
2012-12-04 22:43:52 +0100

    style/presentation fixes
    also fix moderator url

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,7 @@
 function build_site() {
   var content = $('#main_content');
   content.empty();
+  content.append('Index: ');
 
   $.each(version_data, function(program_key, group) {
     content.append(
@@ -15,8 +16,9 @@ function build_site() {
   $.each(version_data, function(group_key, group) {
     var groupContainer = $('<div class="group"></div>');
     groupContainer.append(
-      $('<a></a>').attr('name', group_key),
-      $('<h2></h2>').text(group.short),
+      $('<h2></h2>').text(group.short).append(
+        $('<a>#</a>').attr('href', '#'+group_key).attr('name', group_key)
+      ),
       $('<p></p>').text(group.description)
     );
 
@@ -41,7 +43,7 @@ function build_site() {
           var distro_version_key = distro[version_key];
           if (distro_version_key &&
               program.versions[distro_key] &&
-              program.versions[distro_key][distro_version_key]) {
+              program.versions[distro_key][distro_version_key] !== undefined) {
             var program_distro_version = program.versions[distro_key][distro_version_key];
             var distro_version = distro.versions[distro_version_key];
             if (program_distro_version.version) {
@@ -61,7 +63,7 @@ function build_site() {
                 $('<div class=pv></div>').text(program_distro_version.version).append(quirks)
               );
             } else {
-              td.addClass('unsupported');
+              td.addClass('unsupported').attr('title','unsupported');
             }
           } else {
             td.addClass('unknown');

dc5037a90786df099e4425d7d48642f1a0670208
Evgeni Golov <evgeni@golov.de>
2023-04-23 12:12:04 +0200

    add sles 15.5 as sles "next"

--- data/data.py
+++ data/data.py
@@ -202,6 +202,7 @@ add_centos_data('8', fetch_centos_packages('8-stream'))
 add_centos_data('9', fetch_centos_packages('9-stream', mirror='https://mirror.stream.centos.org/'))
 
 add_suse_data('15', fetch_suse_packages('15.4'))
+add_suse_data('15-next', fetch_suse_packages('15.5'))
 
 with open('data.json', 'w') as jsonfile:
     json.dump({'version_data': version_data, 'distros': data['distros']}, jsonfile, indent=2)
